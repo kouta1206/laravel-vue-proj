c7b86f6b2777b997be25efbc781451e8e21e781e
kouta1206 <ko.bump1206@icloud.com>
2022-11-21 22:14:37 +0900

    いいね機能実装

--- routes/web.php
+++ routes/web.php
@@ -11,4 +11,11 @@
 |
 */
 
-Route::get('/', 'ArticleController@index');
+Route::get('/', 'ArticleController@index')->name('articles.index');
+Route::resource('/articles', 'ArticleController')->except(['index', 'show'])->middleware('auth');
+Route::resource('/articles', 'ArticleController')->only(['show']);
+Auth::routes();
+
+
+
+
